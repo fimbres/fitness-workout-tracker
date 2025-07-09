c76044e4db55420eec7956105ca428e23084adeb
Isaac <fimbresisaac23@gmail.com>
2025-07-09 16:53:36 -0700

    implementing the new auth router

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log("Hey");
+import express from "express";
+import dotenv from "dotenv";
+
+import { authRouter } from "./routes/auth";
+
+import { DB } from "./utils/db";
+
+dotenv.config(); 
+const app = express();
+const db = new DB();
+
+app.use(express.json());
+(async () => {
+    await db.initDatabase();
+})();
+
+app.use('/auth', authRouter);
+
+app.listen(3000, () => {
+    console.log("Listening on port 3000...");
+});
