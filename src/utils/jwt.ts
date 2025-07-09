7d5dcbc1df673fce11bf0081955fec549ee2f0d8
Isaac <fimbresisaac23@gmail.com>
2025-07-09 16:52:55 -0700

    new jwt utils

---
+++ src/utils/jwt.ts
@@ -0,0 +1,16 @@
+import jwt from "jsonwebtoken";
+
+export const createToken = (username: string, userId: string) => {
+    const payload = {
+        userId,
+        username,
+    }
+
+    return jwt.sign(payload, process.env.JWT_SECRET!, {
+        expiresIn: '24h'
+    });
+};
+
+export const verify = (token: string) => {
+    return jwt.verify(token, process.env.JWT_SECRET!, (err) => !Boolean(err));
+}
