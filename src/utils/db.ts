6e2cf56251e2189da7dff799f4f6a735b246a0d5
Isaac <fimbresisaac23@gmail.com>
2025-07-09 18:26:26 -0700

    new db methods on the db handler

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,7 +1,8 @@
 import mysql from 'mysql2/promise';
 import { readFile } from 'fs/promises';
 import { join } from 'path';
-import { User } from '../types/data';
+
+import { User, WorkoutPlan } from '../types/data';
 
 const INIT_PATH = join(__dirname, '..', 'migrations/init.sql');
 const SEED_PATH = join(__dirname, '..', 'migrations/seed.sql');
@@ -71,6 +72,92 @@ export class DB {
             return (result as unknown as { insertId: string }).insertId;
         } catch (error: any) {
             throw new Error(error.message ?? "Unkown error while saving the new user.");
+        }
+    }
+
+    async createNewPlan(user: string, exercises: string[], schedule_time: string) {
+        try {
+            const [result] = await this.db.query(`INSERT INTO WorkoutPlans (user, schedule_time, status) VALUES (?, ?, ?);`, [
+                user,
+                schedule_time,
+                "pending"
+            ]);
+            const insertId = (result as unknown as { insertId: string }).insertId;
+            
+            if(!insertId) throw new Error("Unkown error while saving the new plan."); 
+            
+            const values = exercises.map(e => [insertId, e]);
+
+            await this.db.query(`INSERT INTO WorkoutPlansExercises (workout_plan_id, exercise_id) VALUES ?`, [
+                values
+            ]);
+
+            return insertId;
+        } catch (error: any) {
+            throw new Error(error.message ?? "Unknown error while saving the new plan.");
+        }
+    }
+
+    async updatePlan(exercises: string[], planId: string) {
+        try {
+            await this.db.query(`DELETE FROM WorkoutPlansExercises WHERE workout_plan_id = ?`, [
+                planId
+            ]);
+
+            if(exercises.length > 0) {
+                const values = exercises.map(e => [planId, e]);
+
+                await this.db.query(`INSERT INTO WorkoutPlansExercises (workout_plan_id, exercise_id) VALUES ?`, [
+                    values
+                ]);
+            }
+        } catch (error: any) {
+            throw new Error(error.message ?? "Unknown error while saving the plan.");
+        }
+    }
+
+    async addComment(user: string, planId: string, content: string) {
+        try {
+            await this.db.query(`INSERT INTO Comments (user, workout_plan, content) VALUES (?, ?, ?)`, [
+                user,
+                planId,
+                content
+            ]);
+        } catch (error: any) {
+            throw new Error(error.message ?? "Unknown error while saving the comment.");
+        }
+    }
+
+    async deletePlan(planId: string) {
+        try {
+            await this.db.query(`DELETE FROM WorkoutPlans WHERE id = ?`, [
+                planId
+            ]);
+        } catch (error: any) {
+            throw new Error(error.message ?? "Unknown error while deleting the plan.");
+        }
+    }
+
+    async schedulePlan(planId: string, scheduleTime: string) {
+        try {
+            await this.db.query(`UPDATE WorkoutPlans SET schedule_time = ? WHERE id = ?`, [
+                scheduleTime,
+                planId
+            ]);
+        } catch (error: any) {
+            throw new Error(error.message ?? "Unknown error while scheduling the plan.");
+        }
+    }
+
+    async listPlans(userId: string) {
+        try {
+            const [response] = await this.db.query(`SELECT * FROM WorkoutPlans WHERE user = ?`, [
+                userId
+            ]);
+
+            return (response as WorkoutPlan[]).sort((a, b) => new Date(a.schedule_time).getTime() - new Date(b.schedule_time).getTime());
+        } catch (error: any) {
+            throw new Error(error.message ?? "Unknown error while fetching plans.");
         }
     }
 }
